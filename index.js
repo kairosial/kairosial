40ba180f1a80b8447fec3e5dc8b59bca7d75939f
kairosial <syk.kairos@gmail.com>
2025-04-25 01:29:12 +0900

    Update README.md

--- index.js
+++ index.js
@@ -14,10 +14,13 @@ let text = `
 ### 💻 Skill and Tools
 <p>
     <img alt="Python" src ="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=Python&logoColor=white"/>
-    <img alt="LangChain" src ="https://img.shields.io/badge/LangChain-1C3C3C?style=for-the-badge&logo=LangChain&logoColor=white"/>
     <img alt="PyTorch" src ="https://img.shields.io/badge/PyTorch-EE4C2C?style=for-the-badge&logo=PyTorch&logoColor=white"/>
+    <img alt="LangChain" src ="https://img.shields.io/badge/LangChain-1C3C3C?style=for-the-badge&logo=LangChain&logoColor=white"/>
     <img alt="Flask" src ="https://img.shields.io/badge/Flask-000000?style=for-the-badge&logo=Flask&logoColor=white"/>
+    <img alt="Gradio" src="https://img.shields.io/badge/Gradio-F97316?style=for-the-badge&logo=gradio&logoColor=white"/>
     <img alt="Azure" src ="https://img.shields.io/badge/Azure-0089D6?style=for-the-badge&logoColor=white"/>
+</p>
+<p>
     <img alt="Notion" src ="https://img.shields.io/badge/Notion-000000?style=for-the-badge&logo=Notion&logoColor=white"/>
     <img alt="Slack" src ="https://img.shields.io/badge/Slack-4A154B?style=for-the-badge&logo=Slack&logoColor=white"/>
     <img alt="Git" src ="https://img.shields.io/badge/Git-F05032?style=for-the-badge&logo=Git&logoColor=white"/>
